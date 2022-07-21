5403d63537738b429d91f36357034d8235316fdb
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-07-21 05:02:17 +0000

    chore: increase default loop interval to 0.01s

--- dank_mids/_config.py
+++ dank_mids/_config.py
@@ -3,4 +3,4 @@ import asyncio
 import os
 
 SEMAPHORE = asyncio.Semaphore(int(os.environ.get("DANKMIDSSEMAPHORE", 100_000)))
-LOOP_INTERVAL = float(os.environ.get("DANKMIDSLOOPINTERVAL", 0.001))
+LOOP_INTERVAL = float(os.environ.get("DANKMIDSLOOPINTERVAL", 0.01))
