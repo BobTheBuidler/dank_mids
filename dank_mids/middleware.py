0e4c1f256aed5688f0811a5d8ef2872017d7cad1
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-07-21 05:02:55 +0000

    chore: remove semaphore

--- dank_mids/middleware.py
+++ dank_mids/middleware.py
@@ -4,7 +4,6 @@ from typing import Any, Callable
 from web3 import Web3
 from web3.types import RPCEndpoint, RPCResponse
 
-from dank_mids._config import SEMAPHORE
 from dank_mids.controller import DankMiddlewareController
 from dank_mids.types import AsyncMiddleware
 
@@ -18,7 +17,6 @@ async def dank_middleware(
         if not dank_mids._is_configured and dank_mids.should_batch(method, params):
             await dank_mids._setup()
         if dank_mids.should_batch(method, params):
-            async with SEMAPHORE:
-                return await dank_mids(params)
+            return await dank_mids(params)
         return await make_request(method, params)
     return middleware
