bbc16ee19e5c0e37992ebfa148b943798064fa78
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-10-17 10:42:00 +0000

    feat: improve type hints for brownie_patch module

--- dank_mids/brownie_patch/call.py
+++ dank_mids/brownie_patch/call.py
@@ -1,7 +1,7 @@
 
 import functools
 from types import MethodType
-from typing import Any, Dict, Tuple, Union
+from typing import Any, Dict, Optional, Tuple, Union
 
 import eth_abi
 from brownie.convert.normalize import format_input, format_output
@@ -14,12 +14,12 @@ from multicall.utils import run_in_subprocess
 from web3 import Web3
 
 
-def __encode_input(abi: Dict, signature: str, *args: Tuple) -> str:
+def __encode_input(abi: Dict[str, Any], signature: str, *args: Tuple[Any,...]) -> str:
     data = format_input(abi, args)
     types_list = get_type_strings(abi["inputs"])
     return signature + eth_abi.encode_abi(types_list, data).hex()
 
-def __decode_output(hexstr: str, abi: Dict) -> Any:
+def __decode_output(hexstr: str, abi: Dict[str, Any]) -> Any:
     selector = HexBytes(hexstr)[:4].hex()
     if selector == "0x08c379a0":
         revert_str = eth_abi.decode_abi(["string"], HexBytes(hexstr)[4:])[0]
@@ -42,7 +42,7 @@ def __decode_output(hexstr: str, abi: Dict) -> Any:
     return result
 
 def _patch_call(call: ContractCall, w3: Web3) -> None:
-    async def _encode_input(self, *args: Tuple[Any,...]) -> str:
+    async def _encode_input(self: ContractCall, *args: Tuple[Any,...]) -> str:
         return await run_in_subprocess(
             __encode_input,
             self.abi,
@@ -50,15 +50,15 @@ def _patch_call(call: ContractCall, w3: Web3) -> None:
             *(arg if not hasattr(arg, 'address') else arg.address for arg in args)  # type: ignore
         )
     
-    async def _decode_output(self, data: str) -> Any:
+    async def _decode_output(self: ContractCall, data: str) -> Any:
         return await run_in_subprocess(__decode_output, data, self.abi)
 
     @functools.wraps(call)
     async def coroutine(
-        self,
-        *args: Tuple,
-        block_identifier: Union[int, str, bytes] = None,
-        override: Dict = None
+        self: ContractCall,
+        *args: Tuple[Any,...],
+        block_identifier: Optional[Union[int, str, bytes]] = None,
+        override: Optional[Dict[str, str]] = None
     ) -> Any:
         if override:
             raise ValueError("Cannot use state override with `coroutine`.")
--- dank_mids/brownie_patch/overloaded.py
+++ dank_mids/brownie_patch/overloaded.py
@@ -1,7 +1,8 @@
 import functools
 from types import MethodType
-from typing import Coroutine, Dict, Tuple, Union
+from typing import Any, Coroutine, Dict, Optional, Tuple, Union
 
+from brownie import Contract
 from brownie.network.contract import ContractCall, ContractTx, OverloadedMethod
 from dank_mids.brownie_patch.call import _patch_call
 from web3 import Web3
@@ -10,11 +11,11 @@ from web3 import Web3
 def _patch_overloaded_method(call: OverloadedMethod, w3: Web3) -> None:
     @functools.wraps(call)
     async def coroutine(
-        self,
-        *args: Tuple,
-        block_identifier: Union[int, str, bytes] = None,
-        override: Dict = None
-    ) -> Coroutine:
+        self: Contract,
+        *args: Tuple[Any,...],
+        block_identifier: Optional[Union[int, str, bytes]] = None,
+        override: Optional[Dict[str, str]] = None
+    ) -> Any:
         fn = self._get_fn_from_args(args)
         kwargs = {"block_identifier": block_identifier, "override": override}
         kwargs = {k: v for k, v in kwargs.items() if v is not None}
