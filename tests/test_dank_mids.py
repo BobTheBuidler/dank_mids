b0a5ad69ad145f8ef8cd1d90ef2c9411606d833c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-10-17 20:03:39 +0000

    fix(test) adapt test to recent changes

--- tests/test_dank_mids.py
+++ tests/test_dank_mids.py
@@ -8,9 +8,10 @@ from multicall.utils import await_awaitable, gather
 from tests.fixtures import dank_w3
 
 CHAI = '0x06AF07097C9Eeb7fD685c692751D5C66dB49c215'
-BIG_WORK = [Call(CHAI, 'totalSupply()(uint)', [[f'totalSupply{i}',None]],_w3=dank_w3).coroutine() for i in range(100_000)]
 height = chain.height
-MULTIBLOCK_WORK = [Call(CHAI, 'totalSupply()(uint)', [[f'totalSupply{i}',None]],_w3=dank_w3, block_id=height-i).coroutine() for i in range(100_000)]
+BIG_WORK = [Call(CHAI, 'totalSupply()(uint)', [[f'totalSupply{i}',None]], block_id=height - (i // 25000), _w3=dank_w3).coroutine() for i in range(100_000)]
+height = chain.height
+MULTIBLOCK_WORK = [Call(CHAI, 'totalSupply()(uint)', [[f'totalSupply{i}',None]], _w3=dank_w3, block_id=height-i).coroutine() for i in range(100_000)]
 
 def test_dank_middleware():
     await_awaitable(gather(BIG_WORK))
@@ -21,15 +22,16 @@ def test_dank_middleware():
     assert cid, "The DankMiddlewareController did not process any calls."
     assert mid, "The DankMiddlewareController did not process any batches."
     assert rid, "The DankMiddlewareController did not process any requests."
-    print(f"calls:             {cid}")
-    print(f"multicalls:        {rid}")
-    print(f"batches:           {mid}")
-    print(f"calls per batch:   {cid/mid}")
-    print(f"calls per request:   {cid/rid}")
-    print(f"requests per batch:  {rid/mid}")
+    print(f"calls:                  {cid}")
+    print(f"multicalls:             {mid}")
+    print(f"requests:               {rid}")
+    print(f"calls per multicall:    {cid/mid}")
+    print(f"calls per request:      {cid/rid}")
+    print(f"multicalls per request: {mid/rid}")
     # General "tests" that verify batching performance
-    assert mid < cid / 10, f"Batched {cid} calls into {mid} batches. Performance underwhelming."
-    assert rid < cid / 5,  f"Batched {cid} calls into {rid} multicalls. Performance underwhelming."
+    assert mid < cid / 100, f"Batched {cid} calls into {mid} multicalls. Performance underwhelming."
+    assert rid < cid / 150,  f"Batched {cid} calls into {rid} requests. Performance underwhelming."
+    assert mid / rid > 1,  f"Batched {mid} multicalls into {rid} requests. Performance underwhelming."
 
 def test_bad_hex_handling():
     chainlinkfeed = "0xfe67209f6FE3BA6cE36d0941700085C194e958DF"
@@ -44,21 +46,3 @@ def test_next_mid():
     
 def test_next_bid():
     assert instances[0].worker.multicall_uid.next + 1 == instances[0].worker.multicall_uid.next
-
-def test_json_batching():
-
-    # reset uids
-    dank_mids_controller = instances[0]
-    dank_mids_controller.call_uid._value = 0
-    dank_mids_controller.worker.request_uid._value = 0
-
-    await_awaitable(gather(MULTIBLOCK_WORK))
-    cid = dank_mids_controller.call_uid.latest
-    jid = dank_mids_controller.worker.jsonrpc_batch_uid.latest
-    rid = dank_mids_controller.worker.request_uid.latest
-    assert jid, "The DankMiddlewareController did not process any jsonrpc batches."
-    print(f"jsonrpc batches:    {jid}")
-    print(f'requests:           {rid}')
-    print(f'requests per batch: {rid / jid}')
-    print(f'calls per request   {cid / rid}')
-    print(f'calls per batch     {cid / jid}')
