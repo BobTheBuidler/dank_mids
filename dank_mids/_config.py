94708e05a5f8890e3e306a209868619b2a0f5a04
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-10-17 10:30:48 +0000

    feat: increase default ASYNCIO_TIMEOUT to 2m

--- dank_mids/_config.py
+++ dank_mids/_config.py
@@ -9,6 +9,7 @@ LOOP_INTERVAL = float(os.environ.get("DANKMIDSLOOPINTERVAL", 0.01))
 DEMO_MODE = bool(os.environ.get("DANKMIDS_DEMO_MODE")) or bool(os.environ.get("DANK_MIDS_DEMO_MODE"))
 
 # With default AsyncBaseProvider settings, some dense calls will fail
-#   due to aiohttp.TimeoutError where they would otherwise succeed.
-#   We set the default to 30s but if you're doing serious work you may want to increase it further.
-AIOHTTP_TIMEOUT = ClientTimeout(int(os.environ.get("AIOHTTP_TIMEOUT", 30)))
+#  due to aiohttp.TimeoutError where they would otherwise succeed.
+# We set the default to 2 minutes but if you're doing serious work
+#  you may want to increase it further.
+AIOHTTP_TIMEOUT = ClientTimeout(int(os.environ.get("AIOHTTP_TIMEOUT", 120)))
