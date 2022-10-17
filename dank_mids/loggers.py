944198d69df1bd89a042e194c95a2888a121e9d2
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-10-17 10:04:46 +0000

    chore: remove unused var

--- dank_mids/loggers.py
+++ dank_mids/loggers.py
@@ -6,5 +6,4 @@ main_logger = logging.getLogger("dank_mids")
 demo_logger = logging.getLogger("dank_mids.demo_mode")
 sort_logger = logging.getLogger("dank_mids.should_batch")
 
-dank_mids_lazy_logger = LazyLoggerFactory("DANKMIDS")
 sort_lazy_logger = LazyLoggerFactory("DANKMIDS_SHOULDBATCH")(sort_logger)
