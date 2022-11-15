935f995d85602028f31f6252353fe8047d621df4
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-11-15 23:15:47 +0000

    chore: cleanup

--- dank_mids/semaphore.py
+++ dank_mids/semaphore.py
@@ -13,15 +13,14 @@ class _DummySemaphore:
 
 class Semaphores:
     def __init__(self) -> None:
-        self.method_semaphores = defaultdict(lambda: _DummySemaphore())
-        for method, value in semaphore_envs.items():
-            self.method_semaphores[method] = asyncio.Semaphore(value)
+        self.method_semaphores = {key: asyncio.Semaphore(value) for key, value in semaphore_envs.items()}
         self.keys = self.method_semaphores.keys()
+        self.dummy = _DummySemaphore()
     
     def __getitem__(self, method: RPCEndpoint) -> Union[asyncio.Semaphore, _DummySemaphore]:
         for key in self.keys:
             if key in method:
                 return self.method_semaphores[key]
-        return self.method_semaphores[method]
+        return self.dummy
 
 method_semaphores = Semaphores()
