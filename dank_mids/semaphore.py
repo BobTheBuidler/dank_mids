9452abb87688c567706612cfd0d685613c637b42
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2022-12-28 13:18:08 -0500

    fix: semaphore issues when multi-threading (#37)

--- dank_mids/semaphore.py
+++ dank_mids/semaphore.py
@@ -2,6 +2,33 @@ import asyncio
 from typing import Union
 from web3.types import RPCEndpoint
 from dank_mids._config import semaphore_envs
+from threading import current_thread
+from typing import Dict
+
+
+class ThreadsafeSemaphore:
+    """
+    While its a bit weird to run multiple event loops, sometimes either you or a lib you're using must do so. 
+    When in use in threaded applications, the semaphore will not work as intended but at least your program will function.
+    You may need to reduce the semaphore value for multi-threaded applications.
+    """
+
+    def __init__(self, value: int) -> None:
+        self.default_value = value
+        self.semaphores: Dict[int, asyncio.Semaphore] = {}
+    
+    @property
+    def semaphore(self) -> asyncio.Semaphore:
+        tid = current_thread()
+        if tid not in self.semaphores:
+            self.semaphores[tid] = asyncio.Semaphore(self.default_value)
+        return self.semaphores[tid]
+    
+    async def __aenter__(self):
+        await self.semaphore.acquire()
+    
+    async def __aexit__(self, *args):
+        self.semaphore.release()
 
 
 class _DummySemaphore:
@@ -12,11 +39,11 @@ class _DummySemaphore:
 
 class Semaphores:
     def __init__(self) -> None:
-        self.method_semaphores = {key: asyncio.Semaphore(value) for key, value in semaphore_envs.items()}
+        self.method_semaphores = {key: ThreadsafeSemaphore(value) for key, value in semaphore_envs.items()}
         self.keys = self.method_semaphores.keys()
         self.dummy = _DummySemaphore()
     
-    def __getitem__(self, method: RPCEndpoint) -> Union[asyncio.Semaphore, _DummySemaphore]:
+    def __getitem__(self, method: RPCEndpoint) -> Union[ThreadsafeSemaphore, _DummySemaphore]:
         for key in self.keys:
             if key in method:
                 return self.method_semaphores[key]
