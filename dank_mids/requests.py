2851d6463c18cabd3e9171ab2a63d882d38a5a39
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-03-15 18:39:52 -0400

    fix: generalize err handler

--- dank_mids/requests.py
+++ dank_mids/requests.py
@@ -54,14 +54,14 @@ def _reattempt_call_and_return_exception(target: ChecksumAddress, calldata: byte
 def _err_response(e: Exception) -> RPCError:
     """ Extract an error message from `e` to use in a spoof rpc response. """
     if isinstance(e.args[0], str) or isinstance(e.args[0], RequestInfo):
-        err_msg = f"DankMidsError: {e.__class__.__name__} {e.args}"
+        err_msg = f"DankMidsError: {e.__class__.__name__}: {e.args}"
     elif isinstance(e.args[0], Exception):
-        err_msg = f"DankMidsError: {e.args[0].__class__.__name__} {e.args[0].args}"
-    elif not :
-        err_msg = f"DankMidsError: {e.__class__.__name__} {e.args}"
-    elif hasattr(e.args[0], '__contains__') and "message" in e.args[0]:
+        err_msg = f"DankMidsError: {e.args[0].__class__.__name__}: {e.args[0].args}"
+    elif not hasattr(e.args[0], '__contains__'):
+        err_msg = f"DankMidsError: {e.__class__.__name__}: {e.args}"
+    elif "message" in e.args[0]:
         err_msg = e.args[0]["message"]
-    elif hasattr(e.args[0], '__contains__') and "error" in e.args[0] and hasattr(e.args[0]["error"], '__contains__') and "message" in e.args[0]["error"]:
+    elif "error" in e.args[0] and hasattr(e.args[0]["error"], '__contains__') and "message" in e.args[0]["error"]:
         err_msg = e.args[0]["error"]["message"]
     else:
         raise e
