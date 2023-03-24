39d490ea20fd2c91274985c6cd82e6cf04454e7d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-03-24 01:21:01 -0400

    fix: conflict where 2 project deps both depend on dank + brownie (#50)

--- dank_mids/helpers.py
+++ dank_mids/helpers.py
@@ -15,12 +15,16 @@ from web3.types import Formatters, FormattersDict, RPCEndpoint, RPCResponse
 from dank_mids.middleware import dank_middleware
 from dank_mids.types import AsyncMiddleware
 
+dank_w3s = []
 
 def setup_dank_w3(async_w3: Web3) -> Web3:
     """ Injects Dank Middleware into an async Web3 instance. """
     assert async_w3.eth.is_async and isinstance(async_w3.provider, AsyncBaseProvider)
-    async_w3.middleware_onion.inject(dank_middleware, layer=0)
-    async_w3.middleware_onion.add(geth_poa_middleware)
+    # NOTE: We use this lookup to prevent errs where 2 project dependencies both depend on dank_mids and eth-brownie.
+    if async_w3 not in dank_w3s:
+        async_w3.middleware_onion.inject(dank_middleware, layer=0)
+        async_w3.middleware_onion.add(geth_poa_middleware)
+        dank_w3s.append(async_w3)
     return async_w3
 
 def setup_dank_w3_from_sync(sync_w3: Web3) -> Web3:
