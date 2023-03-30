8131ee53282e4af2d5896eb1b741d696da24c5b6
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-03-29 23:05:29 -0400

    feat: make err msgs useful again

--- dank_mids/brownie_patch/overloaded.py
+++ dank_mids/brownie_patch/overloaded.py
@@ -16,7 +16,15 @@ def _patch_overloaded_method(call: OverloadedMethod, w3: Web3) -> None:
         block_identifier: Optional[Union[int, str, bytes]] = None,
         override: Optional[Dict[str, str]] = None
     ) -> Any:
-        fn = self._get_fn_from_args(args)
+        try:
+            fn = self._get_fn_from_args(args)
+        except ValueError as e:
+            if f"Contract has more than one function '.{call._name}" in str(e) and f"You must explicitly declare which function you are calling, e.g. .{call._name}" in str(e):
+                e = str(e)                    
+                breakpoint = e.find("(*args)")
+                raise ValueError(f"{e[:breakpoint]}.coroutine{e[breakpoint:]}")
+            raise e
+                                        
         kwargs = {"block_identifier": block_identifier, "override": override}
         kwargs = {k: v for k, v in kwargs.items() if v is not None}
         return await fn.coroutine(*args, **kwargs)
@@ -27,5 +35,30 @@ def _patch_overloaded_method(call: OverloadedMethod, w3: Web3) -> None:
     # TODO implement this properly
         #elif isinstance(call, ContractTx):
             #_patch_tx(call, w3)
+    
+    
+    class Coroutine:
+        async def __call__(
+            self: Contract,
+            *args: Tuple[Any,...],
+            block_identifier: Optional[Union[int, str, bytes]] = None,
+            override: Optional[Dict[str, str]] = None
+        ) -> None:
+            try:
+                fn = self._get_fn_from_args(args)
+            except ValueError as e:
+                if f"Contract has more than one function '.{fn.__name__}" in str(e) and f"You must explicitly declare which function you are calling, e.g. .{fn.__name__}" in str(e):
+                    e = str(e)                    
+                    search_for = f"e.g. .{fn.__name__}"
+                    breakpoint = e.find(search_for} + len(search_for)
+                    raise ValueError(f"{e[:breakpoint]}.coroutine{e[breakpoint:]}")
+                raise e
+            kwargs = {"block_identifier": block_identifier, "override": override}
+            kwargs = {k: v for k, v in kwargs.items() if v is not None}
+            return await fn.coroutine(*args, **kwargs)
+        
+        def __getitem__(self, key: Union[Tuple, str]) -> "_ContractMethod":
+            return super().__getitem__(key)
+        
     
     call.coroutine = MethodType(coroutine, call)
