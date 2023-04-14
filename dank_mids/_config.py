5f0068286819b5b498e8ef251ab5723694e1ccfd
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-04-14 17:14:01 +0000

    wip

--- dank_mids/_config.py
+++ dank_mids/_config.py
@@ -17,9 +17,10 @@ GANACHE_FORK = bool(os.environ.get("DANKMIDS_GANACHE_FORK", os.environ.get("DANK
 
 # With default AsyncBaseProvider settings, some dense calls will fail
 #  due to aiohttp.TimeoutError where they would otherwise succeed.
-# We set the default to 2 minutes but if you're doing serious work
+# This is not due to the response time from the node, but the event loop timing.
+# We set the default to 5 minutes but if you're doing serious work
 #  you may want to increase it further.
-AIOHTTP_TIMEOUT = ClientTimeout(int(os.environ.get("AIOHTTP_TIMEOUT", 120)))
+AIOHTTP_TIMEOUT = ClientTimeout(int(os.environ.get("AIOHTTP_TIMEOUT", 300)))
 
 
 # Method-specific Semaphores
