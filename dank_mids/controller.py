0992f46bc4d010e890b3cb3a98edd1746d8b797d
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-04-23 21:47:33 +0000

    fix: try except _futs.remove

--- dank_mids/controller.py
+++ dank_mids/controller.py
@@ -132,7 +132,11 @@ class DankMiddlewareController:
                 """Notifies the controller in the event of daemon shutdown or failure."""
                 self._daemon_running = False
                 if not fut.exception():
-                    self._futs.remove(fut)
+                    try:
+                        self._futs.remove(fut)
+                    except ValueError as e:
+                        if str(e) != "list.remove(x): x not in list":
+                            raise
             fut.add_done_callback(done_callback)
             self._futs.append(fut)
             
@@ -152,7 +156,12 @@ class DankMiddlewareController:
                 main_logger.debug(self._futs)
             for fut in futs:
                 if fut.done() and not fut.exception():
-                    self._futs.remove(fut)
+                    try:
+                        self._futs.remove(fut)
+                    except ValueError as e:
+                        if str(e) != "list.remove(x): x not in list":
+                            raise
+                    
             await asyncio.sleep(5)
         main_logger.debug('exiting exception daemon.')
 
