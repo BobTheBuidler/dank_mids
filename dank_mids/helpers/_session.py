8209921a6458b4916694f1061410adad0a013bc1
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-06-26 04:13:40 +0000

    fix: make session threadsafe

--- dank_mids/helpers/_session.py
+++ dank_mids/helpers/_session.py
@@ -1,11 +1,15 @@
 
 from aiohttp import ClientSession, ClientTimeout
 from async_lru import alru_cache
+from threading import get_ident
 
 from dank_mids import _config
 
 
-@alru_cache(maxsize=1)
 async def get_session() -> ClientSession:
+    return await _get_session(get_ident())
+
+@alru_cache(maxsize=None)
+async def _get_session(thread_ident: int) -> ClientSession:
     timeout = ClientTimeout(_config.AIOHTTP_TIMEOUT)
     return ClientSession(timeout=timeout, raise_for_status=True)
