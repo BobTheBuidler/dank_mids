797a34ab50ee30359ddc654234334c581b337543
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-06-26 04:02:19 +0000

    fix: ClientTimeout

--- dank_mids/helpers/_session.py
+++ dank_mids/helpers/_session.py
@@ -1,5 +1,5 @@
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientTimeout
 from async_lru import alru_cache
 
 from dank_mids import _config
@@ -7,4 +7,4 @@ from dank_mids import _config
 
 @alru_cache(maxsize=1)
 async def get_session() -> ClientSession:
-    return ClientSession(timeout=_config.AIOHTTP_TIMEOUT, raise_for_status=True)
+    return ClientSession(timeout=ClientTimeout(_config.AIOHTTP_TIMEOUT), raise_for_status=True)
