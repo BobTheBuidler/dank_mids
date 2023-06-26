b3a0785b105f488124748bb1c80fb3ed8175e93a
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-06-26 20:10:23 +0000

    fix: JSONRPCBatch bisect_and_retry batch already started err

--- dank_mids/requests.py
+++ dank_mids/requests.py
@@ -527,16 +527,13 @@ class JSONRPCBatch(_Batch[Union[Multicall, RPCRequest]]):
         return responses
     
     async def bisect_and_retry(self) -> None:
-        batches = [
+        await await_all(
             Multicall(self.worker, chunk[0].calls, f"json{self.jid}_{i}")  # type: ignore [misc]
             if len(chunk) == 1 and isinstance(chunk[0], Multicall)
             else JSONRPCBatch(self.worker, chunk, f"{self.jid}_{i}")
             for i, chunk in enumerate(self.bisected)
             if chunk
-        ]
-        for batch in batches:
-            batch._started = True
-        await await_all(batches)
+        )
 
     def _post_future_cleanup(self) -> None:
         self.controller.pending_rpc_calls = JSONRPCBatch(self.worker)
