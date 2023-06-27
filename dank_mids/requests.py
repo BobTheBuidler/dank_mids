23482b64fbc910738a11f84605d017e0224e8048
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-06-27 22:24:52 +0000

    feat: add eth_getBalance to RETURN_TYPES for faster decoding

--- dank_mids/requests.py
+++ dank_mids/requests.py
@@ -113,6 +113,7 @@ class _RequestMeta(Generic[_Response], metaclass=abc.ABCMeta):
 
 RETURN_TYPES = {
     "eth_call": str,
+    "eth_getBalance": str,
     "eth_blockNumber": str,  # TODO: see if we can decode this straight to an int
     #"eth_getBlockByNumber": Dict[str, Union[str, List[str, object]]],
 }
