adde363ea77bab23bb0c4a5d35b207cbb8a0b6e6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-06-29 15:07:18 +0000

    chore: fix comment

--- dank_mids/_config.py
+++ dank_mids/_config.py
@@ -12,9 +12,7 @@ DEMO_MODE = bool(os.environ.get("DANKMIDS_DEMO_MODE", os.environ.get("DANK_MIDS_
 # Are you calling a ganache fork? Can't use state override code
 GANACHE_FORK = bool(os.environ.get("DANKMIDS_GANACHE_FORK", os.environ.get("DANK_MIDS_GANACHE_FORK")))
 
-# With default AsyncBaseProvider settings, some dense calls will fail
-#  due to aiohttp.TimeoutError where they would otherwise succeed.
-# We set the default to 2 minutes but if you're doing serious work
+# We set the default to 5 minutes but if you're doing serious work
 #  you may want to increase it further.
 AIOHTTP_TIMEOUT = int(os.environ.get("AIOHTTP_TIMEOUT", 300))
 
