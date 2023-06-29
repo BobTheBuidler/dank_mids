b7d04d422eca1e9f40b01ffc85de0f9f075f2e92
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-06-29 05:39:38 +0000

    chore: reduce debug logging

--- dank_mids/types.py
+++ dank_mids/types.py
@@ -106,7 +106,7 @@ class PartialResponse(_DictStruct):
     def decode_result(self, method: Optional[str] = None, _caller = None) -> Any:
         # NOTE: These must be added to the `RETURN_TYPES` constant above manually
         if method and (typ := RETURN_TYPES.get(method)):
-            if method in ["eth_blockNumber", "eth_chainId"]:
+            if method in ["eth_blockNumber", "eth_chainId", "eth_getCode", "eth_getBlockByNumber"]:
                 return decode(self.result, type=typ)
             try:
                 from time import time
