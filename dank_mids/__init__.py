3147f2d79cad1b7a6cd3247671d2054d111f1fa0
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-07-02 08:10:58 +0000

    chore: fix init

--- dank_mids/__init__.py
+++ dank_mids/__init__.py
@@ -1,5 +1,4 @@
 
 from dank_mids.controller import instances
-from dank_mids.helpers import (executor, semaphore, setup_dank_w3,
-                               setup_dank_w3_from_sync)
+from dank_mids.helpers import setup_dank_w3, setup_dank_w3_from_sync
 from dank_mids.middleware import dank_middleware
