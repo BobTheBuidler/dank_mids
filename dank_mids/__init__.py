c40e25bf89f5b566ca2c7e8a9cec5c013e318aae
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-07-03 20:35:32 +0000

    bump conc.futures queue size from 1

--- dank_mids/__init__.py
+++ dank_mids/__init__.py
@@ -1,4 +1,12 @@
 
+
+
 from dank_mids.controller import instances
 from dank_mids.helpers import setup_dank_w3, setup_dank_w3_from_sync
 from dank_mids.middleware import dank_middleware
+
+def _configure_concurrent_future_work_queue_size():
+    import concurrent.futures.process as _cfp
+    _cfp.EXTRA_QUEUED_CALLS = 50_000
+
+_configure_concurrent_future_work_queue_size()
