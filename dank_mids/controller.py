0bc53700e7a8137abcd9e0960adadb2f7fbde19f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2023-07-26 03:09:30 -0400

    feat: env to init controller with full request
    DANKMIDS_USE_FULL_REQUEST

--- dank_mids/controller.py
+++ dank_mids/controller.py
@@ -1,5 +1,6 @@
 
 import logging
+import os
 import threading
 from collections import defaultdict
 from typing import Any, DefaultDict, List, Literal, Optional
@@ -20,7 +21,7 @@ from dank_mids.batch import DankBatch
 from dank_mids.helpers import decode, session
 from dank_mids.requests import JSONRPCBatch, Multicall, RPCRequest, eth_call
 from dank_mids.semaphores import MethodSemaphores
-from dank_mids.types import BlockId, ChainId, PartialRequest, RawResponse
+from dank_mids.types import BlockId, ChainId, PartialRequest, RawResponse, Request
 from dank_mids.uid import UIDGenerator, _AlertingRLock
 
 logger = logging.getLogger(__name__)
@@ -46,7 +47,7 @@ class DankMiddlewareController:
 
         self.chain_id = self.sync_w3.eth.chain_id
         # NOTE: We need this mutable for node types that require the full jsonrpc spec
-        self.request_type = PartialRequest
+        self.request_type = PartialRequest if not os.environ.get("DANKMIDS_USE_FULL_REQUEST") else Request
         self._time_of_request_type_change = 0
         self.state_override_not_supported: bool = ENVS.GANACHE_FORK or self.chain_id == 100  # Gnosis Chain does not support state override.
 
