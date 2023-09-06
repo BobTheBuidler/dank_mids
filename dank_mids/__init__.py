93bd97de607814bac8b3518dfe541e55cfffc2cb
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-09-06 23:58:19 +0000

    chore: remove no longer necessary hack fix

--- dank_mids/__init__.py
+++ dank_mids/__init__.py
@@ -1,7 +1,4 @@
 
-
-
-from dank_mids._how_is_this_real import _the_most_absurd_fix_youve_ever_seen
 from dank_mids.controller import instances
 from dank_mids.helpers import setup_dank_w3, setup_dank_w3_from_sync
 from dank_mids.middleware import dank_middleware
@@ -12,4 +9,3 @@ def _configure_concurrent_future_work_queue_size():
     _cfp.EXTRA_QUEUED_CALLS = 50_000
 
 _configure_concurrent_future_work_queue_size()
-_the_most_absurd_fix_youve_ever_seen()
