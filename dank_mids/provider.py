ee00edad7faddfacd92777bf6022fdb876167aa4
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-09-28 01:13:36 +0000

    feat: tweak default concurrency settings

--- dank_mids/provider.py
+++ dank_mids/provider.py
@@ -31,8 +31,8 @@ class DankProvider:
         self,
         controller: "DankMiddlewareController", 
         w3: Web3, 
-        min_concurrency: int = 24, 
-        max_concurrency: int = 64,
+        min_concurrency: int = 32, 
+        max_concurrency: int = 256,
     ) -> None:
         self.controller = controller
         endpoint = w3.provider.endpoint_uri
