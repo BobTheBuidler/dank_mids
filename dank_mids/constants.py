8f7b5dfc4eb73a324efd4ac65a9526cad5016342
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-01-10 02:32:37 -0500

    chore: cleanup logging of missing trie node errs (#121)

--- dank_mids/constants.py
+++ dank_mids/constants.py
@@ -4,7 +4,7 @@ import multicall
 from multicall.constants import Network
 
 TOO_MUCH_DATA_ERRS = ["Payload Too Large", "content length too large", "request entity too large", "batch limit exceeded"]
-RETRY_ERRS = ["connection reset by peer", "server disconnected", "execution aborted (timeout = 5s)", "batch limit exceeded"]
+RETRY_ERRS = ["connection reset by peer", "server disconnected", "execution aborted (timeout =", "batch limit exceeded"]
 
 GAS_LIMIT = multicall.constants.GAS_LIMIT
 MULTICALL2_OVERRIDE_CODE = multicall.constants.MULTICALL2_BYTECODE
