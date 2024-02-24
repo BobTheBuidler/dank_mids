2c0af55dbd91d412a62063d32695c52c60675886
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-02-24 09:14:22 +0000

    feat: convenience import BlockSemaphore

--- dank_mids/__init__.py
+++ dank_mids/__init__.py
@@ -2,6 +2,7 @@
 from dank_mids.controller import instances
 from dank_mids.helpers import setup_dank_w3, setup_dank_w3_from_sync
 from dank_mids.middleware import dank_middleware
+from dank_mids.semaphores import BlockSemaphore
 
 try:
     from dank_mids.brownie_patch import Contract, dank_eth, dank_web3, patch_contract
