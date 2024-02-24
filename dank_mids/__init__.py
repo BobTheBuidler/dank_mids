479f74514c034d0e7d2258df1b27ab5ce0a82cbc
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-02-24 04:14:46 -0500

    feat: convenience imports (#126)
    * feat: convenience aliases
    
    * feat: convenience import BlockSemaphore

--- dank_mids/__init__.py
+++ dank_mids/__init__.py
@@ -2,9 +2,14 @@
 from dank_mids.controller import instances
 from dank_mids.helpers import setup_dank_w3, setup_dank_w3_from_sync
 from dank_mids.middleware import dank_middleware
+from dank_mids.semaphores import BlockSemaphore
 
 try:
-    from dank_mids.brownie_patch import Contract, dank_web3, patch_contract
+    from dank_mids.brownie_patch import Contract, dank_eth, dank_web3, patch_contract
+    from web3.eth import AsyncEth as _AsyncEth
+    # aliased for cleanliness and convenience
+    web3 = dank_web3
+    eth = dank_eth
 except ImportError:
     pass
 
