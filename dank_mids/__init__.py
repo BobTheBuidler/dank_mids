44862c48a64abd0b592f54d9d60085798f1d3ef9
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-04-10 16:38:57 -0400

    feat: define module contents (#156)

--- dank_mids/__init__.py
+++ dank_mids/__init__.py
@@ -5,12 +5,17 @@ from dank_mids.helpers import setup_dank_w3, setup_dank_w3_from_sync
 from dank_mids.middleware import dank_middleware
 from dank_mids.semaphores import BlockSemaphore
 
+__all__ = ["dank_middleware", "BlockSemaphore", "setup_dank_w3", "setup_dank_w3_from_sync"]
+
 try:
     from dank_mids.brownie_patch import Contract, dank_eth, dank_web3, patch_contract
     from web3.eth import AsyncEth as _AsyncEth
     # aliased for cleanliness and convenience
     web3 = dank_web3
     eth = dank_eth
+
+    __all__ += ["Contract", "dank_web3", "web3", "dank_eth", "eth", "patch_contract"]
+
 except ImportError:
     pass
 
