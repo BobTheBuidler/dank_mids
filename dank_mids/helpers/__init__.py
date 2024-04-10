2ede1614037d9650e8c13c99a49d09964c54f839
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-10 20:41:54 +0000

    chore: make public members private

--- dank_mids/helpers/__init__.py
+++ dank_mids/helpers/__init__.py
@@ -1,3 +1,5 @@
 
-from dank_mids.helpers.helpers import (await_all, setup_dank_w3,
+from dank_mids.helpers._helpers import (await_all, setup_dank_w3,
                                        setup_dank_w3_from_sync)
+
+__all__ = ["setup_dank_w3", "setup_dank_w3_from_sync"]
