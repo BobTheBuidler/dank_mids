5c269fc898fda3be7e37c62e75777dbaeb639f8d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-04-10 15:56:17 -0400

    feat(docs): hide private members

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,6 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 autodoc_default_options = {
-    'private-members': True,
     'special-members': ','.join([
         '__init__',
         '__call__',
