53fb9a052c7fae4f744f4caa362db9bf651a9945
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-19 20:24:39 +0000

    fix: segment output file by chain

--- dank_mids/_debugging/failures.py
+++ dank_mids/_debugging/failures.py
@@ -1,8 +1,12 @@
 
+from datetime import datetime
+
+from brownie import chain
+
 from dank_mids._debugging._base import _CSVWriter
 
 class FailedRequestWriter(_CSVWriter):
-    filename = "/failed_requests.csv"
+    filename = f"/{chain.id}_{datetime.now().timestamp()}_failed_requests.csv"
     async def record_failure(self, e: Exception, request_data: bytes):
         row = type(e).__name__, str(e), request_data.hex()
         await self.write_row(','.join(row))
