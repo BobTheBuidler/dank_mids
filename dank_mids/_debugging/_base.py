d83c5ed1f9026056a16891c039be0a34ab329929
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-20 04:00:42 +0000

    fix: failure log path

--- dank_mids/_debugging/_base.py
+++ dank_mids/_debugging/_base.py
@@ -11,14 +11,14 @@ from aiofiles.threadpool.text import AsyncTextIOWrapper
 logger = logging.getLogger("dank_mids.debugging")
 
 class _FileHelper(metaclass=abc.ABCMeta):
-    path = "~/.dank_mids/debug"
+    path = f"{os.path.expanduser( '~' )}/.dank_mids/debug"
     def __init__(self):
         self.ensure_dir()
     @lru_cache(maxsize=1)
     def ensure_dir(cls) -> None:
         os.makedirs(cls.path, exist_ok=True)
-    def open(self) -> AiofilesContextManager[None, None, AsyncTextIOWrapper]:
-        logger.info("opening %s with mode %s", self.uri, self.mode)
+    def open(self) -> "AiofilesContextManager[None, None, AsyncTextIOWrapper]":
+        print("opening %s with mode %s", self.uri, self.mode)
         return aiofiles.open(self.uri, self.mode)
     @abc.abstractproperty
     def uri(self) -> str:
@@ -34,7 +34,7 @@ class _CSVWriter(_FileHelper):
         return self.path + self.filename
     async def write_row(self, row: str) -> None:
         async with self.open() as file:
-            logger.info("writing row %s to file %s", row, file)
+            print("writing row %s to file %s", row, file)
             await file.write(row)
     @abc.abstractproperty
     def filename(self) -> str:
--- dank_mids/_debugging/failures.py
+++ dank_mids/_debugging/failures.py
@@ -1,14 +1,23 @@
 
 from datetime import datetime
+from functools import cached_property, lru_cache
 
-from brownie import chain
+from a_sync import ProcessingQueue
 
 from dank_mids._debugging._base import _CSVWriter
 
+@lru_cache(maxsize=None)
 class FailedRequestWriter(_CSVWriter):
-    filename = f"/{chain.id}_{datetime.now().timestamp()}_failed_requests.csv"
-    async def record_failure(self, e: Exception, request_data: bytes):
+    def __init__(self, chainid: int):
+        super().__init__()
+        self.chainid = chainid
+        self.record_failure = ProcessingQueue(self._record_failure, num_workers=1, return_data=True)
+    @cached_property
+    def filename(self) -> str:
+        return f"/{self.chainid}_{int(datetime.now().timestamp())}_failed_requests.csv"
+    async def _record_failure(self, e: Exception, request_data: bytes):
         row = type(e).__name__, str(e), request_data.hex()
         await self.write_row(','.join(row))
 
-record = FailedRequestWriter().record_failure
+def record(chainid: int, e: Exception, request_data: bytes) -> None:
+    return FailedRequestWriter(chainid).record_failure(e, request_data)
