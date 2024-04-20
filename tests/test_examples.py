158cbe2195c58150024730439d3f86dbee24cfb1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-04-20 02:27:31 -0400

    feat(test): test examples (#176)

---
+++ tests/test_examples.py
@@ -0,0 +1,12 @@
+
+import pytest
+
+import eth_retry
+
+import examples.dank_brownie_example
+
+
+@pytest.mark.asyncio_cooperative
+@eth_retry.auto_retry  # retry etherscan rate limit errors
+async def test_dank_brownie_example():
+    await examples.dank_brownie_example._main()
