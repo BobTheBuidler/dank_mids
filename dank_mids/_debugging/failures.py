a8fda4b637db5fcfb631f93a246a54cd2d67286e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-20 04:07:10 +0000

    feat: separate debug files by failure type

--- dank_mids/_debugging/failures.py
+++ dank_mids/_debugging/failures.py
@@ -8,16 +8,19 @@ from dank_mids._debugging._base import _CSVWriter
 
 @lru_cache(maxsize=None)
 class FailedRequestWriter(_CSVWriter):
-    def __init__(self, chainid: int):
+    def __init__(self, failure_type: Type[Exception], chainid: int):
         super().__init__()
+        self.failure_type = failure_type
         self.chainid = chainid
         self.record_failure = ProcessingQueue(self._record_failure, num_workers=1, return_data=True)
     @cached_property
     def filename(self) -> str:
-        return f"/{self.chainid}_{int(datetime.now().timestamp())}_failed_requests.csv"
+        return f"/{self.chainid}/{int(datetime.now().timestamp())}_{self.failure_type.__name__}s.csv"
     async def _record_failure(self, e: Exception, request_data: bytes):
-        row = type(e).__name__, str(e), request_data.hex()
+        if not isinstance(e, self.failure_type):
+            raise TypeError(e, self.failure_type)
+        row = str(e), request_data.hex()
         await self.write_row(','.join(row))
 
 def record(chainid: int, e: Exception, request_data: bytes) -> None:
-    return FailedRequestWriter(chainid).record_failure(e, request_data)
+    return FailedRequestWriter(type(e), chainid).record_failure(e, request_data)
