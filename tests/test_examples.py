d6b5b25383bcdfff8da96641c77f8c65ead78152
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-20 06:22:07 +0000

    fix(test): fix test example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,8 +1,16 @@
 
 import pytest
 
+import brownie
+import eth_retry
+
 import examples.dank_brownie_example
 
+
 @pytest.mark.asyncio_cooperative
+@eth_retry.auto_retry  # retry etherscan rate limit errors
 async def test_dank_brownie_example():
+    with pytest.raises(ValueError):
+        await examples.dank_brownie_example._main()
+    brownie._CONFIG.settings['autofetch_sources'] = True
     await examples.dank_brownie_example._main()
