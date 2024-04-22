bd20d534898a8ca16925a28dd825e31d5a1aedf9
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-04-22 17:02:13 -0400

    fix: failed request writer (#179)

--- dank_mids/_debugging/failures.py
+++ dank_mids/_debugging/failures.py
@@ -18,7 +18,7 @@ class FailedRequestWriter(_CSVWriter):
         if not issubclass(failure_type, BaseException):
             raise TypeError(f"`failure_type` must be an Exception type. You passed {failure_type}")
         self.failure_type = failure_type
-        self.record_failure = ProcessingQueue(self._record_failure, num_workers=1, return_data=True)
+        self.record_failure = ProcessingQueue(self._record_failure, num_workers=1, return_data=False)
     @cached_property
     def filename(self) -> str:
         return f"{int(datetime.now().timestamp())}_{self.failure_type.__name__}s.csv"
@@ -28,4 +28,4 @@ class FailedRequestWriter(_CSVWriter):
         await self.write_row(request_type, request_uid, request_length, e, request_data)
 
 def record(chainid: int, e: Exception, request_type: str, request_uid: Union[int, str], request_length: Union[int, Literal["unknown"]], request_data: Union[List["Request"], List["PartialRequest"], bytes]) -> None:
-    FailedRequestWriter(chainid, type(e)).record_failure(e, request_type, request_uid, request_data)
+    FailedRequestWriter(chainid, type(e)).record_failure(e, request_type, request_uid, request_length, request_data)
