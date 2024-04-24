32490f5178af2a2ac0e00470e765a38e39d1fff1
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-04-24 00:30:00 +0000

    fix: unencodable type

--- dank_mids/helpers/_codec.py
+++ dank_mids/helpers/_codec.py
@@ -1,19 +1,14 @@
 
 from msgspec import Raw, json
 
-from dank_mids.types import (Any, AttributeDict, PartialResponse, RawResponse,
-                             _JSONRPCBatchResponse, nested_dict_of_stuff)
+from dank_mids.types import (Any, PartialResponse, RawResponse,
+                             JSONRPCBatchResponseRaw, nested_dict_of_stuff, _encode_hook)
 
 raw = lambda data: RawResponse(json.decode(data, type=Raw))
 nested_dict = lambda data: json.decode(data, type=nested_dict_of_stuff)
-jsonrpc_batch = lambda data: decoded if isinstance(decoded := json.decode(data, type=_JSONRPCBatchResponse), PartialResponse) else _map_raw(decoded)
+jsonrpc_batch = lambda data: decoded if isinstance(decoded := json.decode(data, type=JSONRPCBatchResponseRaw), PartialResponse) else _map_raw(decoded)
 
 _map_raw = lambda decoded: list(map(RawResponse, decoded))
 
 def encode(obj: Any) -> bytes:
     return json.encode(obj, enc_hook=_encode_hook)
-
-def _encode_hook(obj: Any) -> Any:
-    if isinstance(obj, AttributeDict):
-        return dict(obj)
-    raise NotImplementedError(type(obj))
