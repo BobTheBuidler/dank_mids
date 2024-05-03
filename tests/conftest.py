81f2fc60a97f697456de8b5e6dd71bb0fa53a44d
Patrick Daly <pmdaly12@gmail.com>
2024-05-03 15:05:26 -0700

    local load env check

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,9 @@ from brownie import network
 from dotenv import load_dotenv
 
 
-load_dotenv()
+if "CI" not in os.environ:
+    # running locally (not in github actions)
+    load_dotenv()
 
 sys.path.insert(0, os.path.abspath('.'))
 
