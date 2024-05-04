50284c2e5ce5754f5b5939bc1eedf4742fe23cf9
pmdaly <pmdaly@users.noreply.github.com>
2024-05-04 14:41:55 -0700

    feat: poetry (#182)
    * poetry checkpoint
    
    * use .env file
    
    * tox example
    
    * adding coverage and pytest qol
    
    * chore: update pyproject.toml
    
    * chore: bump ez-a-sync to needed version
    
    * adding asyncio
    
    * updating lockfile
    
    * release workflow setup with poetry
    
    * updating mypy actions
    
    * updating pytest.yaml w/ poetry
    
    * docs workflow updated with poetry
    
    * dev comment
    
    * chore: use matrix for mypy action
    
    * local load env check
    
    * brownie should be accessed correctly now
    
    ---------
    
    Co-authored-by: BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,16 @@ import sys
 
 import brownie
 from brownie import network
+from dotenv import load_dotenv
+
+
+if "CI" not in os.environ:
+    # running locally (not in github actions)
+    load_dotenv()
 
 sys.path.insert(0, os.path.abspath('.'))
 
 brownie._CONFIG.settings['autofetch_sources'] = True
 
 if not network.is_connected():
-    network.connect(os.environ["PYTEST_NETWORK"])
+    network.connect(os.getenv("PYTEST_NETWORK"))
