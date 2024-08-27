2d8e3906c1703a4fd30078e48f432c23a66be582
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-08-27 13:51:27 -0400

    feat: remove previously deprecated modules (#208)

--- dank_mids/executor.py
+++
@@ -1,6 +0,0 @@
-
-import logging
-logger = logging.getLogger(__name__)
-logger.warning("dank_mids.executor module has been deprecated and will be removed eventually.")
-logger.warning("you can now import what you need from a_sync.primitives module https://github.com/BobTheBuidler/ez-a-sync")
-from a_sync.primitives.executor import *
