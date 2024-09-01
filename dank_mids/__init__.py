5324209c33cee6fb29d6de9cce0cf0914d0c6a2e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-09-01 23:23:29 +0000

    fix: broken import

--- dank_mids/__init__.py
+++ dank_mids/__init__.py
@@ -1,6 +1,6 @@
 from contextlib import suppress
 
-from dank_mids.brownie_patch import DankContractCall, DankContractMethod, DankContractTx, DankOverloadedMethod, dank_web3
+from dank_mids.brownie_patch import DankContractCall, DankContractMethod, DankContractTx, DankOverloadedMethod
 from dank_mids.controller import instances
 from dank_mids.helpers import setup_dank_w3, setup_dank_w3_from_sync
 from dank_mids.middleware import dank_middleware
@@ -10,7 +10,6 @@ __all__ = ["dank_middleware", "BlockSemaphore", "setup_dank_w3", "setup_dank_w3_
 
 with suppress(ImportError):
     from dank_mids.brownie_patch import Contract, dank_eth, dank_web3, patch_contract
-    from web3.eth import AsyncEth as _AsyncEth
     # aliased for cleanliness and convenience
     web3 = dank_web3
     eth = dank_eth
