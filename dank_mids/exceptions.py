42feb91b5ffc8de01a21087acf0941a315ee4bfa
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-09-02 01:04:13 +0000

    feat: raise more specific BrownieNotConnected error

--- dank_mids/exceptions.py
+++ dank_mids/exceptions.py
@@ -1,3 +1,10 @@
 
 class Revert(ValueError):
     ...
+
+class BrownieNotConnectedError(RuntimeError):
+    """
+    :class:`RuntimeError` raised when brownie is not connected but needs to be in order to access functionality within :mod:`dank_mids.brownie_patch`.
+    """
+    def __init__(self):
+        super().__init__("brownie is not connected to a network")
