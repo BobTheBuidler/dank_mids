bd783eabf8b17571bfa57a5f70adcde3a8f0a18f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-09-02 05:18:02 -0400

    feat: raise more specific BrownieNotConnected error (#224)
    * feat: raise more specific BrownieNotConnected error

--- dank_mids/__init__.py
+++ dank_mids/__init__.py
@@ -2,21 +2,15 @@ from contextlib import suppress
 
 from dank_mids.brownie_patch import DankContractCall, DankContractMethod, DankContractTx, DankOverloadedMethod
 from dank_mids.controller import instances
+from dank_mids.exceptions import BrownieNotConnectedError
 from dank_mids.helpers import setup_dank_w3, setup_dank_w3_from_sync
 from dank_mids.middleware import dank_middleware
 from dank_mids.semaphores import BlockSemaphore
 
 __all__ = ["dank_middleware", "BlockSemaphore", "setup_dank_w3", "setup_dank_w3_from_sync"]
 
-with suppress(ImportError):
-    from dank_mids.brownie_patch import Contract, dank_eth, dank_web3, patch_contract
-    # aliased for cleanliness and convenience
-    web3 = dank_web3
-    eth = dank_eth
 
-    __all__ += ["Contract", "dank_web3", "web3", "dank_eth", "eth", "patch_contract"]
-
-
+# Configure concurrent.futures.process queue size
 def _configure_concurrent_future_work_queue_size():
     """
     Configures the concurrent futures process pool to allow for a larger number of queued calls.
@@ -29,3 +23,28 @@ def _configure_concurrent_future_work_queue_size():
     _cfp.EXTRA_QUEUED_CALLS = 50_000
 
 _configure_concurrent_future_work_queue_size()
+
+
+# Import brownie objects
+__brownie_objects = ["Contract", "dank_web3", "web3", "dank_eth", "eth", "patch_contract"]
+with suppress(ImportError):
+    from dank_mids.brownie_patch import Contract, dank_eth, dank_web3, patch_contract
+    __all__ += __brownie_objects
+    # aliased for cleanliness and convenience
+    web3 = dank_web3
+    eth = dank_eth
+
+
+# Define custom getattr
+def __getattr__(name: str):
+    """
+    Custom attribute access handler for the 'dank_mids' module.
+
+    This method is called when an attribute that is not found in the module's namespace
+    is accessed. It allows for custom error handling for brownie-specific objects.
+    """
+    if name in __brownie_objects:
+        raise BrownieNotConnectedError(name)
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+
--- dank_mids/exceptions.py
+++ dank_mids/exceptions.py
@@ -1,3 +1,16 @@
 
 class Revert(ValueError):
     ...
+
+class BrownieNotConnectedError(RuntimeError):
+    """
+    :class:`RuntimeError` raised when brownie is not connected but needs to be in order to access functionality within :mod:`dank_mids.brownie_patch`.
+    """
+    def __init__(self, obj_name: str):
+        """
+        Initializes a new BrownieNotConnectedError.
+        
+        Params:
+            obj_name: The name of the object the user attempted to access, which requires brownie to be connected.
+        """
+        super().__init__(f"Brownie is not connected to a network. You must be connected to a network in order to access `dank_mids.{obj_name}`.")
