03e779f6288c8c041725fd0b2107f0bd9b03b4a4
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-15 08:46:55 +0000

    fix: trace Action struct

--- dank_mids/structs/trace.py
+++ dank_mids/structs/trace.py
@@ -1,6 +1,7 @@
 
 from decimal import Decimal
 from functools import cached_property
+from typing import Literal, Optional
 
 from hexbytes import HexBytes
 from msgspec import UNSET, Raw, field, json
@@ -9,27 +10,15 @@ from dank_mids.structs.data import Address, uint, _decode_hook
 from dank_mids.structs.dict import DictStruct, LazyDictStruct
 
 
-class Action(DictStruct, frozen=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
-    rewardType: str
-
-#class Result(DictStruct, frozen=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
-    
-
-class FilterTrace(LazyDictStruct, frozen=True, kw_only=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
-    blockNumber: uint
-    transactionHash: HexBytes
-    transactionPosition: int
+class Action(LazyDictStruct, frozen=True, kw_only=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
+    callType: Literal["call", "delegatecall"]
     _sender: Address = field(name="from")
     _to: Address
-    _address: Raw = field(name="address", default=UNSET)
+    _author: Address = field(name="author", default=UNSET)
+    input: HexBytes
+    gas: uint
+    rewardType: Optional[Literal["block", "uncle"]] = None
     _value: Raw = field(name="value", default=UNSET)
-    _gas: Raw = field(name="gas", default=UNSET)
-    _action: Raw = field(name="action", default=UNSET)
-    _result: Raw = field(name="result", default=UNSET)
-
-    @property
-    def block(self) -> uint:
-        return self.blockNumber
 
     @cached_property
     def sender(self) -> Address:
@@ -40,21 +29,38 @@ class FilterTrace(LazyDictStruct, frozen=True, kw_only=True, forbid_unknown_fiel
         return json.decode(self._to, type=Address, dec_hook=Address._decode_hook)
 
     @cached_property
-    def address(self) -> Address:
-        return json.decode(self._address, type=Address, dec_hook=Address._decode_hook)
+    def author(self) -> Address:
+        return json.decode(self._author, type=Address, dec_hook=Address._decode_hook)
 
     @cached_property
     def value(self) -> Decimal:
         return Decimal(json.decode(self._value, type=uint, dec_hook=uint._decode_hook)) / 10 ** 18
 
+class Result(DictStruct, frozen=True, kw_only=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
+    output: HexBytes
+    gasUsed: uint
+
+class FilterTrace(LazyDictStruct, frozen=True, kw_only=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
+    blockNumber: uint
+    blockHash: HexBytes
+    transactionHash: HexBytes
+    transactionPosition: int
+    _address: Raw = field(name="address", default=UNSET)
+    _action: Raw = field(name="action", default=UNSET)
+    _result: Raw = field(name="result", default=UNSET)
+
+    @property
+    def block(self) -> uint:
+        return self.blockNumber
+
     @cached_property
-    def gas(self) -> uint:
-        return json.decode(self._gas, type=uint, dec_hook=uint._decode_hook)
+    def address(self) -> Address:
+        return json.decode(self._address, type=Address, dec_hook=Address._decode_hook)
 
     @cached_property
     def action(self) -> Action:
         return json.decode(self._action, type=Action, dec_hook=_decode_hook)
 
     @cached_property
-    def result(self) -> str:
-        return json.decode(self._result, type=str, dec_hook=_decode_hook)
+    def result(self) -> Result:
+        return json.decode(self._result, type=Result, dec_hook=_decode_hook)
