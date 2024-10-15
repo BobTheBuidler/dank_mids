a7907a52fb27065eaeab27af063b3884429d53b0
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-15 18:58:52 +0000

    fix: uint decode for non-string

--- dank_mids/structs/data.py
+++ dank_mids/structs/data.py
@@ -3,6 +3,7 @@ from typing import Type
 
 from enum import IntEnum
 from hexbytes import HexBytes
+from msgspec import ValidationError
 
 from cachetools.func import ttl_cache
 from eth_utils import to_checksum_address
@@ -23,6 +24,14 @@ class uint(int):
     @classmethod
     def _decode_hook(cls, typ: Type["uint"], obj: str):
         return cls(obj, 16)
+    @classmethod
+    def _decode(cls, obj) -> "uint":
+        try:
+            return cls(obj, 16)
+        except ValidationError as e:
+            if "int cant convert non-string with explicit base" in str(e):
+                return cls(obj)
+            raise
 
 class Status(IntEnum):
     Revert = 0
@@ -34,7 +43,7 @@ class Status(IntEnum):
     def _decode(cls, obj) -> "Status":
         try:
             return cls(int(obj, 16))
-        except TypeError:
+        except TypeError as e:
             if str(e) == "int() can't convert non-string with explicit base":
                 return Status(int(obj))
             raise
@@ -45,7 +54,7 @@ def _decode_hook(typ: Type, obj: str):
     elif typ is Address:
         return checksum(obj)
     elif typ is uint:
-        return uint(obj, 16)
+        return uint._decode(obj)
     elif typ is Status:
         return Status._decode(obj)
     raise TypeError(typ)
