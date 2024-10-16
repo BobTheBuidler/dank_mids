947dcbcb3a17d3d1612cdbd3416d44cb5c11b03e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-16 13:51:35 +0000

    fix: always decode all log fields

--- dank_mids/structs/log.py
+++ dank_mids/structs/log.py
@@ -1,47 +1,27 @@
 
-from functools import cached_property
 from typing import List, Optional
 
 from hexbytes import HexBytes
-from msgspec import Raw, field, json
 
-from dank_mids.structs.data import Address, uint, _decode_hexbytes
+from dank_mids.structs.data import Address, uint
 from dank_mids.structs.dict import LazyDictStruct
 
 
 class TinyLog(LazyDictStruct, frozen=True, kw_only=True):  # type: ignore [call-arg]
 
-    _topics: Raw = field(name="topics")
+    topics: List[HexBytes]
     """
     An array of 0 to 4 32-byte topics. 
     The first topicis the event signature and the others are indexed filters on the event return data.
     """
 
-    @cached_property
-    def topics(self) -> Optional[List[HexBytes]]:
-        """
-        An array of 0 to 4 32-byte topics. 
-        The first topicis the event signature and the others are indexed filters on the event return data.
-        """
-        return json.decode(self._topics, type=Optional[List[HexBytes]], dec_hook=_decode_hexbytes)
-
 class SmallLog(TinyLog, frozen=True, kw_only=True):  # type: ignore [call-arg]
 
-    _address: Raw = field(name="address")
+    address: Optional[Address]
     """The address of the contract that generated the log."""
 
-    _data: Raw = field(name="data")
+    data: Optional[HexBytes]
     """Array of 32-bytes non-indexed return data of the log."""
-
-    @cached_property
-    def address(self) -> Optional[Address]:
-        """The address of the contract that generated the log."""
-        return json.decode(self._address, type=Optional[Address], dec_hook=Address._decode_hook)
-    
-    @cached_property
-    def data(self) -> Optional[HexBytes]:
-        """Array of 32-bytes non-indexed return data of the log."""
-        return json.decode(self._data, type=Optional[HexBytes], dec_hook=_decode_hexbytes)
 
 
 class Log(SmallLog, frozen=True, kw_only=True):  # type: ignore [call-arg]
@@ -69,10 +49,5 @@ class Log(SmallLog, frozen=True, kw_only=True):  # type: ignore [call-arg]
 
 class FullLog(Log, frozen=True, kw_only=True, forbid_unknown_fields=True):  # type: ignore [call-arg]
 
-    _blockHash: Raw = field(name="blockHash")
+    blockHash: Optional[HexBytes]
     """The hash of the block where the transaction was included where the log originated from. `None` for pending transactions."""
-
-    @cached_property
-    def blockHash(self) -> Optional[HexBytes]:
-        """The hash of the block where the transaction was included where the log originated from. `None` for pending transactions."""
-        return json.decode(self._blockHash, type=Optional[HexBytes], dec_hook=_decode_hexbytes)
