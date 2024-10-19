cbe9b4031cc46475db8e04fc860244dd92e757b8
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-19 03:53:10 +0000

    fix: syntax

--- dank_mids/_requests.py
+++ dank_mids/_requests.py
@@ -137,7 +137,7 @@ class RPCRequest(_RequestMeta[RawResponse]):
         with self.controller.pools_closed_lock:
             if isinstance(self, eth_call) and self.multicall_compatible:
                 self.controller.pending_eth_calls[self.block].append(self)
-            elif self.method.startswith("trace", "debug"):
+            elif self.method.startswith(("trace", "debug")):
                 raise NotImplementedError("we should not get here", self)
             else:
                 self.controller.pending_rpc_calls.append(self)
