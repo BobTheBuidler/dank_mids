4264e3273f41a50effa93fc2cbb5c27b7b704e63
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-19 20:38:51 +0000

    feat: import more stuff to structs main

--- dank_mids/structs/__init__.py
+++ dank_mids/structs/__init__.py
@@ -4,6 +4,12 @@ from dank_mids.structs.dict import DictStruct, LazyDictStruct
 from dank_mids.structs.log import Log
 from dank_mids.structs.receipt import TransactionReceipt
 from dank_mids.structs.trace import FilterTrace
-from dank_mids.structs.transaction import Transaction
+from dank_mids.structs.transaction import Transaction, Transaction1559, Transaction2930, TransactionLegacy
 
-__all__ = ["Block", "Transaction", "TransactionReceipt", "Log", "DictStruct", "LazyDictStruct", "ErigonHeader", "FilterTrace"]
+__all__ = [
+    # main stuff
+    "Block", "Transaction", "TransactionReceipt", "Log", "DictStruct", "LazyDictStruct", "ErigonHeader", "FilterTrace",
+
+    # transaction subtypes    
+    "TransactionLegacy", "Transaction2930", "Transaction1559",
+]
