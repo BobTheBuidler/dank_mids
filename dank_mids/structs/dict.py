b92da1d515480bca9b67ae50955e9c27e4c2250b
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-19 18:30:56 +0000

    fix: init subclass

--- dank_mids/structs/dict.py
+++ dank_mids/structs/dict.py
@@ -134,5 +134,7 @@ class DictStruct(Struct, dict=True):
 class LazyDictStruct(DictStruct, frozen=True, dict=True):  # type: ignore [call-arg]
     def __init_subclass__(cls, *args, **kwargs):
         super().__init_subclass__(*args, **kwargs)
+        if cls.__name__ == "StructMeta":  # AttributeError: 'msgspec._core.StructMeta' object has no attribute '__struct_fields__'.
+            return
         resolved_fields = tuple(field[1:] if field[0] == '_' else field for field in cls.__struct_fields__)
         cls.__struct_fields__ = resolved_fields
