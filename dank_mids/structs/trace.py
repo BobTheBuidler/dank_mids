1bc3106f5f1228b0878b1d2c41c7dfff329b0793
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-20 16:16:37 +0000

    fix: init call trace

--- dank_mids/structs/trace.py
+++ dank_mids/structs/trace.py
@@ -122,7 +122,7 @@ class _FilterTraceBase(LazyDictStruct, frozen=True, kw_only=True, forbid_unknown
         # make sure the obj is hashable
         force_setattr(self, "traceAddress", tuple(self.traceAddress))
 
-class CallTrace(_FilterTraceBase, tag="create", frozen=True, kw_only=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):
+class CallTrace(_FilterTraceBase, tag="call", frozen=True, kw_only=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):
     type: ClassVar[Literal["call"]] = "call"
     action: Call
 
