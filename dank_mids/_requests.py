324c3525aeb67018de6a202a47abc418a410eda1
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-12-17 12:13:17 -0400

    fix: _get_len_for_method internal helper (#324)

--- dank_mids/_requests.py
+++ dank_mids/_requests.py
@@ -169,7 +169,7 @@ def _get_len_for_method(method: str) -> int:
     # NOTE: These are totally arbitrary, used to reduce frequency of giant batches/responses
     if method == "eth_getTransactionReceipt":
         return 5
-    elif any(m in method for m in ["eth_getCode" "eth_getBlockBy", "eth_getTransaction"]):
+    elif method in {"eth_getTransaction", "eth_getCode"} or "eth_getBlockBy" in method:
         return 3
     return 1
 
