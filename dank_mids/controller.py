5d73f1a1484c141b556ba1a102bf6a252e708788
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-01-07 14:45:41 -0500

    Update controller.py

--- dank_mids/controller.py
+++ dank_mids/controller.py
@@ -139,7 +139,7 @@ class DankMiddlewareController:
         if self.mc3:
             self.no_multicall.add(self.mc3.address)
 
-        self.eth_call_semaphores: BlockSemaphore = ENVS.method_semaphores["eth_call"]
+        self.eth_call_semaphores: BlockSemaphore = ENVS.method_semaphores["eth_call"]  # type: ignore [assignment]
         """Used for managing concurrency of eth_calls."""
 
         # semaphores soon to be deprecated for smart queue
