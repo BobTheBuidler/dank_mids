ebf8b8100571338dcc3845975ce7ccfd08b62efe
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-01-07 16:36:07 -0500

    fix: AttributeError: 'DankMiddlewareController' object has no attribute 'method_semaphores' (#354)

--- dank_mids/_requests.py
+++ dank_mids/_requests.py
@@ -39,6 +39,7 @@ from weakref import ref as weak_ref
 import a_sync
 import eth_retry
 from a_sync import AsyncProcessPoolExecutor, PruningThreadPoolExecutor
+from a_sync.primitives import DummySemaphore
 from aiohttp.client_exceptions import ClientResponseError
 from eth_abi import abi, decoding
 from eth_typing import ChecksumAddress
@@ -92,6 +93,9 @@ _Response = TypeVar(
 
 class RPCError(_RPCError, total=False):
     dankmids_added_context: Dict[str, Any]
+
+
+_dummy_semaphore = DummySemaphore()
 
 
 _super_init = a_sync.Event.__init__
@@ -381,7 +385,7 @@ class RPCRequest(_RequestMeta[RawResponse]):
         # NOTE: We cannot cache this property so the semaphore control pattern in the `duplicate` fn will work as intended
         if self.method == "eth_call":
             return self.controller.eth_call_semaphores[self.params[1]]
-        return self.controller.method_semaphores[self.method]  # type: ignore [return-value]
+        return _dummy_semaphore
 
     async def create_duplicate(self) -> RPCResponse:
         # Not actually self, but for typing purposes it is.
