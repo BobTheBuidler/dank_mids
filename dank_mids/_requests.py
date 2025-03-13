74b09876ccd5adcd6493b7d94f3363b10f579813
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-03-13 05:39:06 +0000

    chore: add batch details to breaking excs

--- dank_mids/_requests.py
+++ dank_mids/_requests.py
@@ -862,9 +862,13 @@ class Multicall(_Batch[RPCResponse, eth_call]):
             if isinstance(result, Exception):
                 if not isinstance(result, DankMidsInternalError):
                     _log_error(
-                        "That's not good, there was an exception in a %s. These are supposed to be handled.\n%s\n",
+                        "That's not good, there was an exception in a %s. These are supposed to be handled.\n"
+                        "Exc: %s\n"
+                        "%s contents: %s\n\n",
                         batch.__class__.__name__,
                         result,
+                        batch.__class__.__name__,
+                        list(batch),
                         exc_info=True,
                     )
                 raise result
@@ -1197,9 +1201,13 @@ class JSONRPCBatch(_Batch[RPCResponse, Union[Multicall, RPCRequest]]):
             if isinstance(result, Exception):
                 if not isinstance(result, DankMidsInternalError):
                     _log_error(
-                        "That's not good, there was an exception in a %s. These are supposed to be handled.\n%s\n",
+                        "That's not good, there was an exception in a %s. These are supposed to be handled.\n"
+                        "Exc: %s\n"
+                        "%s contents: %s\n\n",
                         batch.__class__.__name__,
                         result,
+                        batch.__class__.__name__,
+                        list(batch),
                         exc_info=True,
                     )
                 raise result
