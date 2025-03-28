4ebe9162873dfe864018771f9e65bf3968e818cc
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-03-28 07:58:45 +0000

    feat: faster formatters

--- dank_mids/_web3/abi.py
+++ dank_mids/_web3/abi.py
@@ -1,9 +1,8 @@
-from functools import partial
 from typing import Any, Callable, Sequence, Tuple
 
 from eth_typing import TypeStr
 from eth_utils.toolz import compose, curry
-from web3._utils.abi import abi_sub_tree, data_tree_map, strip_abi_type
+from web3._utils.abi import ABITypedData, abi_sub_tree, strip_abi_type
 from web3._utils.formatters import recursive_map
 
 
@@ -55,11 +54,38 @@ def get_mapper(
         pipeline = [
             # 1. Decorating the data tree with types
             # web3.py implementation is `abi_data_tree(types)` but a lambda is faster than a curried func call
-            lambda data: list(map(abi_sub_tree, types, data)),
+            lambda data: iterator(map(abi_sub_tree, types, data)),
             # 2. Recursively mapping each of the normalizers to the data
-            *map(data_tree_map, normalizers),
+            *map(get_data_tree_map, normalizers),
             # 3. Stripping the types back out of the tree
             strip_abi_types,
         ]
         mapper = _mappers[(normalizers, types)] = compose(*pipeline.__reversed__())
     return mapper
+
+
+class iterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+    def __iter__(self):
+        return self
+    def __next__(self):
+        return next(self.iterator)
+
+_data_tree_maps = {}
+
+def get_data_tree_map(func: Callable[[TypeStr, Any], Tuple[TypeStr, Any]]) -> Callable[[Any], ABITypedData]:
+    f = _data_tree_maps.get(func)
+    if f is None:
+
+        def map_to_typed_data(elements: Any) -> ABITypedData:
+            if isinstance(elements, ABITypedData) and elements.abi_type is not None:
+                return ABITypedData(func(*elements))
+            else:
+                return elements
+        
+        typed_data_func = lambda data: recursive_map(map_to_typed_data, data)
+
+        f = _data_tree_maps[func] = typed_data_func
+
+    return f
