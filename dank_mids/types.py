3482f536655d813903b58541e1259e242f859998
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-03-31 20:41:37 +0000

    fix: arg should be kwarg

--- dank_mids/types.py
+++ dank_mids/types.py
@@ -289,21 +289,11 @@ class PartialResponse(DictStruct, frozen=True, omit_defaults=True, repr_omit_def
                             return result
 
                     elif e.args[0] == "Object contains unknown field `withdrawals`":
-                        return better_decode(
-                            self.result,
-                            type=ShanghaiCapellaBlock,
-                            dec_hook=_decode_hook,
-                            method=method,
-                        )
+                        typ = ShanghaiCapellaBlock
                     
                     elif e.args[0] == "Object contains unknown field `difficulty`":
                         # I've only seen this on OP stack so far, not sure what difficulty means for their chain
-                        return better_decode(
-                            self.result,
-                            BaseBlock,
-                            dec_hook=_decode_hook,
-                            method=method,
-                        )
+                        typ = BaseBlock
                     
                     else:
                         raise
