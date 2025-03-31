4e705d2e704c38ed79ef040d9a72aba537bef9c6
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-03-31 19:03:44 +0000

    chore: raise `EVM error: InvalidFEOpcode` as revert

--- dank_mids/types.py
+++ dank_mids/types.py
@@ -217,7 +217,7 @@ class PartialResponse(DictStruct, frozen=True, omit_defaults=True, repr_omit_def
         message = self.error.message
         if "out of gas" in message:
             return OutOfGas(self)
-        if "execution reverted" in message:
+        if "execution reverted" in message or message == "EVM error: InvalidFEOpcode":
             return ExecutionReverted(self)
         if self.payload_too_large:
             return PayloadTooLarge(self)
