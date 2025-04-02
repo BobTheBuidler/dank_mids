029ee306a92299cadb4aef59d59c1c531049fff4
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-02 15:18:50 -0400

    chore: log traceback for early exits for debugging purposes (#510)
    * chore: log traceback for early exits for debugging purposes
    
    * chore: `black .`
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- dank_mids/_requests.py
+++ dank_mids/_requests.py
@@ -775,8 +775,12 @@ class Multicall(_Batch[RPCResponse, eth_call]):
 
     async def get_response(self) -> None:  # type: ignore [override]
         if self._started:
-            logger.error("%s early exit", self)
+            try:
+                raise RuntimeError(f"{self} early exit")
+            except RuntimeError as e:
+                logger.exception(e)
             return
+
         self._started = True
         if (l := len(self)) == 1:
             return await self._exec_single_call()
