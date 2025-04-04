df29cfe82b59ab0b0f7d4cf0a80176d56ce5b39a
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-04 16:28:12 -0400

    chore: add current task to early exit err log (#529)

--- dank_mids/_requests.py
+++ dank_mids/_requests.py
@@ -1148,7 +1148,7 @@ class JSONRPCBatch(_Batch[RPCResponse, Union[Multicall, eth_call, RPCRequest]]):
 
     async def get_response(self) -> None:  # type: ignore [override]
         if self._awaited:
-            _log_warning("%s exiting early. This shouldn't really happen bro", self)
+            _log_warning("%s exiting early. This shouldn't really happen bro    current task: %s", self, current_task())
             return
         self._awaited = True
 
