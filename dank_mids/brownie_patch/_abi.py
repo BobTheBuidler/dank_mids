50f05e92bc4fa4faff769586b1acb74640f5d6c7
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-04-20 10:33:16 +0000

    fix: expected FunctionABI got FunctionABI

--- dank_mids/brownie_patch/_abi.py
+++ dank_mids/brownie_patch/_abi.py
@@ -4,7 +4,6 @@ from typing import Any
 from brownie.convert.utils import build_function_selector, build_function_signature
 
 
-@functools.lru_cache(maxsize=None)
 class FunctionABI:
     """
     A singleton class to hold function ABI information.
@@ -38,3 +37,8 @@ class FunctionABI:
         The function selector (4-byte signature) of the function.
         This is used in Ethereum transactions to identify which function to call.
         """
+
+    @functools.lru_cache(maxsize=None)
+    @classmethod
+    def singleton(cls, **abi: Any) -> "FunctionABI":
+        return FunctionABI(**abi)
