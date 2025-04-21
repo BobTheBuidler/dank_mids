dab9b744b0cc539f6eac1d2d7a708bc9fc97b7c7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-21 01:28:24 -0400

    chore: extract code from _requests.py to helpers/_codec.py (#608)
    * chore: extract code from _requests.py to helpers/_codec.py
    
    * chore: `black .`
    
    * Update _codec.py
    
    * Update _codec.py
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- dank_mids/helpers/_codec.py
+++ dank_mids/helpers/_codec.py
@@ -1,13 +1,18 @@
-from msgspec import DecodeError, Raw, json
+from itertools import accumulate, chain
+from typing import Any, Callable, Iterable, List, Union, Tuple
+
+from eth_abi.abi import default_codec
+from eth_abi.decoding import ContextFramesBytesIO
+from eth_abi.encoding import DynamicArrayEncoder, TupleEncoder, encode_uint_256
+from msgspec import DecodeError, Raw
+from msgspec.json import decode as json_decode
+from msgspec.json import encode as json_encode
 
 from dank_mids.types import (
-    Any,
-    Callable,
     JSONRPCBatchResponseRaw,
-    List,
+    MulticallChunk,
     PartialResponse,
     RawResponse,
-    Union,
     _encode_hook,
     _nested_dict_of_stuff,
 )
@@ -24,13 +29,13 @@ def decode_raw(data: bytes) -> RawResponse:
         The decoded data wrapped in a RawResponse object.
     """
     try:
-        return RawResponse(json.decode(data, type=Raw))
+        return RawResponse(json_decode(data, type=Raw))
     except DecodeError as e:
         e.args = *e.args, data
         raise
 
 
-decode_nested_dict = lambda data: json.decode(data, type=_nested_dict_of_stuff)
+decode_nested_dict = lambda data: json_decode(data, type=_nested_dict_of_stuff)
 """
 Decode json-encoded bytes into a nested dictionary.
 
@@ -52,7 +57,7 @@ def decode_jsonrpc_batch(data: bytes) -> Union[PartialResponse, List[RawResponse
     Returns:
         Either a PartialResponse if there's an error, or a list of RawResponse objects.
     """
-    decoded = json.decode(data, type=JSONRPCBatchResponseRaw)
+    decoded = json_decode(data, type=JSONRPCBatchResponseRaw)
     return decoded if isinstance(decoded, PartialResponse) else _map_raw(decoded)
 
 
@@ -83,4 +88,59 @@ def encode(obj: Any) -> bytes:
     See Also:
         :func:`msgspec.json.encode`: The underlying JSON encoding function.
     """
-    return json.encode(obj, enc_hook=_encode_hook)
+    return json_encode(obj, enc_hook=_encode_hook)  # type: ignore [no-any-return]
+
+
+# multicall specific stuff
+
+_mcall_encoder: Callable[[Tuple[bool, Iterable[MulticallChunk]]], bytes]  # TupleEncoder
+_mcall_encoder = default_codec._registry.get_encoder("(bool,(address,bytes)[])")
+_array_encoder: DynamicArrayEncoder = _mcall_encoder.encoders[-1]  # type: ignore [attr-defined]
+_item_encoder: TupleEncoder = _array_encoder.item_encoder
+
+# We don't need to follow the validation code from eth-abi since we guarantee the input types
+_mcall_encoder.validate_value = _array_encoder.validate_value = _item_encoder.validate_value = lambda *_: ...  # type: ignore [attr-defined, method-assign]
+
+
+def __encode_new(values: Iterable[MulticallChunk]) -> bytes:
+    encoded_elements, num_elements = __encode_elements_new(values)
+    return encode_uint_256(num_elements) + encoded_elements  # type: ignore [no-any-return]
+
+
+def __encode_elements_new(values: Iterable[MulticallChunk]) -> Tuple[bytes, int]:
+    tail_chunks = tuple(map(_item_encoder, values))
+    count = len(tail_chunks)
+    head_length = 32 * count
+    tail_offsets = chain((0,), accumulate(map(len, tail_chunks[:-1])))
+    head_chunks = map(encode_uint_256, map(head_length.__add__, tail_offsets))
+    return b"".join(chain(head_chunks, tail_chunks)), count
+
+
+_array_encoder.encode = __encode_new  # type: ignore [method-assign]
+_array_encoder.encode_elements = __encode_elements_new  # type: ignore [method-assign]
+
+_mcall_decoder = default_codec._registry.get_decoder("(uint256,uint256,(bool,bytes)[])").decode
+
+
+def mcall_encode(data: Iterable[MulticallChunk]) -> bytes:
+    return _mcall_encoder((False, data))
+
+
+# maybe use this success flag to do something later
+Success = bool
+__get_bytes = lambda tup: tup[1]
+
+
+def mcall_decode(data: PartialResponse) -> Union[List[bytes], Exception]:
+    try:
+        decoded: List[Tuple[Success, bytes]] = _mcall_decoder(
+            ContextFramesBytesIO(data.decode_result("eth_call"))
+        )[
+            2
+        ]  # type: ignore [arg-type]
+    except Exception as e:
+        # NOTE: We need to safely bring any Exceptions back out of the ProcessPool
+        e.args = (*e.args, data.decode_result() if isinstance(data, PartialResponse) else data)
+        return e
+    else:
+        return list(map(__get_bytes, decoded))
