fa86afe9fad72564b6aaf2d269180de460cf854e
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-22 14:20:45 -0400

    chore(mypy): mark WeakList._refs as Final (#619)

--- dank_mids/helpers/_weaklist.py
+++ dank_mids/helpers/_weaklist.py
@@ -14,7 +14,7 @@ _call_ref: Final[Callable[[Ref[_T]], Optional[_T]]] = weakref.ref.__call__
 class WeakList(Generic[_T]):
     def __init__(self, data: Optional[Iterable[_T]] = None) -> None:
         # Mapping from object ID to weak reference
-        self._refs: Dict[int, Ref[_T]] = {}
+        self._refs: Final[Dict[int, Ref[_T]]] = {}
         if data is not None:
             self.extend(data)
 
