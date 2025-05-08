8f98a91f65d37f9ae99d9c84e28fdc38152e9640
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-08 15:30:26 -0400

    Update _abi.py

--- dank_mids/brownie_patch/_abi.py
+++ dank_mids/brownie_patch/_abi.py
@@ -3,6 +3,7 @@ from weakref import WeakValueDictionary
 
 from brownie.convert import utils
 
+from dank_mids.brownie_patch._nocompile import _FunctionABI
 from dank_mids.helpers import _helpers
 
 
@@ -17,8 +18,14 @@ build_function_signature: Final = utils.build_function_signature
 _make_hashable: Final = _helpers._make_hashable
 
 
+# NOTE we must inherit from a python object in order for weakrefs to work, so for now we inherit from _FunctionABI
+# https://github.com/python/cpython/blob/9546eeea90c8deb8570a0ef621f075c3c766bc12/Include/internal/pycore_object.h#L874
+# https://github.com/python/mypy/blob/501a07b45af8e44eda665e53526fc590dc5a014e/mypyc/irbuild/vtable.py#L17
+# https://github.com/python/mypy/blob/501a07b45af8e44eda665e53526fc590dc5a014e/mypyc/codegen/emitclass.py#L285
+# TODO: fix the above
+
 @final
-class FunctionABI:
+class FunctionABI(_FunctionABI):
     """
     A class to hold function ABI information.
 
@@ -51,10 +58,6 @@ class FunctionABI:
         The function selector (4-byte signature) of the function.
         This is used in Ethereum transactions to identify which function to call.
         """
-
-        self.__dict__: Final[Dict[str, Any]] = {}
-        """This __dict__ attr is needed for weakrefs to work."""
-        # self.__weakref__: Optional["ReferenceType[FunctionABI]"] = None
 
     @staticmethod
     def singleton(**abi: Any) -> "FunctionABI":
