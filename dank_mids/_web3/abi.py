5f659812751e255dc07093f527935b45209307fa
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-17 03:55:31 -0400

    Update abi.py

--- dank_mids/_web3/abi.py
+++ dank_mids/_web3/abi.py
@@ -54,61 +54,75 @@ def map_abi_data(
     return get_mapper(normalizers, types)(data)
 
 
-_mappers: Final[Dict[MapperKey, Callable[..., List[Any]]]] = {}
+_mappers: Final[Dict[MapperKey, "_Mapper"]] = {}
 
 
 # web3py builds the pipeline every call, we cache it here instead
 def get_mapper(
     normalizers: Tuple[Normalizer, ...],
     types: Tuple[TypeStr, ...],
-) -> Callable[..., List[Any]]:
+) -> "_Mapper":
     mapper = _mappers.get((normalizers, types))
     if mapper is None:
-        pipeline = (
-            # 1. Decorating the data tree with types
-            # web3.py implementation is `abi_data_tree(types)` but a lambda is faster than a curried func call
-            lambda data: map(abi_sub_tree, types, data),
-            # 2. Recursively mapping each of the normalizers to the data
-            *map(get_data_tree_map, normalizers),
-            # 3. Stripping the types back out of the tree
-            strip_abi_types,
-            list,
-        )
-        mapper = _mappers[(normalizers, types)] = compose(*reversed(pipeline))
+        mapper = _mappers[(normalizers, types)] = _Mapper(normalizers, types)
     return mapper
 
 
-_data_tree_maps: Final[Dict[DataTreeFunc, Callable[[Any], abi.ABITypedData]]] = {}
+@final
+class _Mapper:
+    def __init__(
+        self,
+        normalizers: Tuple[Normalizer, ...],
+        types: Tuple[TypeStr, ...],
+    ):
+        # TODO: vendor ABITypedData and cache some stuff from web3py
+        self.normalizers: Final = [get_data_tree_map(n) for n in normalizers]
+        self.types: Final = types
+    def __call__(self, data: Any) -> List[Any]:
+        # 1. Decorating the data tree with types
+        # TODO: vendor abi_sub_tree
+        data = map(abi_sub_tree, self.types, self.data)
+        # 2. Recursively mapping each of the normalizers to the data
+        for n in self.normalizers:
+            data = n(data)
+        # 3. Stripping the types back out of the tree
+        return list(strip_abi_types(data))
+        
+
+
+_data_tree_maps: Final[Dict[DataTreeFunc, "map_to_typed_data"]] = {}
 
 
 def get_data_tree_map(
     func: DataTreeFunc,
-) -> Callable[[Any], abi.ABITypedData]:
+) -> "map_to_typed_data":
     f = _data_tree_maps.get(func)
     if f is None:
-
-        def map_to_typed_data(elements: Any) -> abi.ABITypedData:
-            datatype = type(elements)
-            if datatype is map or datatype is list:
-                return [map_to_typed_data(obj) for obj in elements]
-            elif datatype is tuple:
-                return tuple(map_to_typed_data(obj) for obj in elements)
-            elif isinstance(elements, Mapping):
-                return datatype((key, map_to_typed_data(val)) for key, val in elements.items())  # type: ignore [call-arg]
-            elif not isinstance(elements, (bytes, str, bytearray)) and isinstance(
-                elements, Iterable
-            ):
-                return datatype(map(map_to_typed_data, elements))
-            elif isinstance(elements, ABITypedData) and elements.abi_type is not None:
-                return ABITypedData(func(*elements))
-            else:
-                return elements
-
-        f = _data_tree_maps[func] = map_to_typed_data
-
+        f = _data_tree_maps[func] = map_to_typed_data(func)
     return f
 
 
+@final
+class map_to_typed_data:
+    def __init__(self, func: DataTreeFunc) -> None:
+        self.func: Final = func
+    def __call__(self, elements: Any) -> Any:
+        datatype = type(elements)
+        if datatype is map or datatype is list:
+            return [self(obj) for obj in elements]
+        elif datatype is tuple:
+            return tuple(self(obj) for obj in elements)
+        elif isinstance(elements, Mapping):
+            return datatype((key, self(val)) for key, val in elements.items())  # type: ignore [call-arg]
+        elif not isinstance(elements, (bytes, str, bytearray)) and isinstance(
+            elements, Iterable
+        ):
+            return datatype(map(self, elements))
+        elif isinstance(elements, ABITypedData) and elements.abi_type is not None:
+            return ABITypedData(self.func(*elements))
+        else:
+            return elements
+        
 def strip_abi_types(data: Any) -> Any:
     datatype = type(data)
     if datatype is map or datatype is list:
