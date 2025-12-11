31e349980ded38bacc9fa0bc5101d5d16c704982
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-12-11 00:46:50 -0400

    Update hashing.py

--- dank_mids/helpers/hashing.py
+++ dank_mids/helpers/hashing.py
@@ -18,8 +18,6 @@ from typing import (
     final,
 )
 
-from mypy_extensions import mypyc_attr
-
 
 TKey = TypeVar("TKey", bound=Hashable)
 TValue = TypeVar("TValue")
@@ -43,7 +41,6 @@ def make_hashable(obj: Any) -> Hashable:
 
 
 @final
-@mypyc_attr(native_class=False)
 class AttributeDict(Mapping[TKey, TValue]):
     """
     Provides superficial immutability, someone could hack around it
