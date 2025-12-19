ebaae84fcb08939bd5c25fcac481c821f8987b16
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-12-19 08:10:47 +0000

    fix: dont compile try_for_result

--- dank_mids/_tasks.py
+++ dank_mids/_tasks.py
@@ -18,55 +18,10 @@ logger: Final = getLogger("dank_mids.tasks")
 log_task_exception: Final = logger.exception
 
 CancelledError: Final = asyncio.CancelledError
-current_task: Final = asyncio.current_task
 ensure_future: Final = asyncio.tasks.ensure_future
 wait_for: Final = asyncio.wait_for
 
 create_task: Final = a_sync.create_task
-
-
-class InternalTaskCancelledError(Exception):
-    """
-    This is a special Exception type that we use to wrap asyncio.CancelledError
-    exceptions that occur where they shouldn't, so they are raised up to the
-    end-user instead of being handled by asyncio's internal CancelledError logic.
-    """
-
-
-async def try_for_result(fut: asyncio.Future[T]) -> T:
-    # If this coroutine is cancelled, it will also cancel the underlying Future.
-    try:
-        return await wait_for(shield(fut), TIMEOUT_SECONDS_BIG)
-    except CancelledError as e:
-        waiter = current_task()
-        if waiter is not None and waiter.cancelled():
-            cancel_message = e.args[0] if e.args else None
-            fut.cancel(
-                f"{waiter} cancellation propagated by dank_mids._tasks.try_for_result: {cancel_message}"
-            )
-        elif fut.cancelled():
-            # This shouldn't happen since we use shield here, we need to re-raise
-            # it as a special exc type so the outer task isn't cancelled by asyncio
-            raise InternalTaskCancelledError(*e.args) from e
-        raise
-
-
-async def try_for_result_quick(fut: asyncio.Future[T]) -> T:
-    # If this coroutine is cancelled, it will also cancel the underlying Future.
-    try:
-        return await wait_for(shield(fut), TIMEOUT_SECONDS_SMALL)
-    except CancelledError as e:
-        waiter = current_task()
-        if waiter is not None and waiter.cancelled():
-            cancel_message = e.args[0] if e.args else None
-            fut.cancel(
-                f"{waiter} cancellation propagated by dank_mids._tasks.try_for_result_quick: {cancel_message}"
-            )
-        elif fut.cancelled():
-            # This shouldn't happen since we use shield here, we need to re-raise
-            # it as a special exc type so the outer task isn't cancelled by asyncio
-            raise InternalTaskCancelledError(*e.args) from e
-        raise
 
 
 def create_batch_task(a: Awaitable[T], name: str) -> asyncio.Task[T]:
